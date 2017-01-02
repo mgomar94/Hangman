4914f6ed7eb0580c598961284c55c526d23beb85
Mohammed Omar <mgomar94@gmail.com>
2017-01-02 08:50:36 +0000

    Version 1.1 - Various small additions

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Hangman
-A simple implementation of the game Hangman
+A simple implementation of the game Hangman 
+
+User inputs a desired word length and an input text file and the program will 
+randomly select a viable word for the user to guess letters at a time until the 
+guessing limit is reached.
--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <fstream>
 #include <string>
+#include <stdio.h>
+#include <stdlib.h> 
 
 
 int main(int argc, char* argv[]) {
@@ -14,6 +16,68 @@ int main(int argc, char* argv[]) {
         return -1;
     }
     
+    int wordLength = argv[1];
     
+    
+    /* Open the file */
+    ifstream in;
+    in.open(argv[2]);
+
+    /* Check if input file was actually opened */
+    if(!in.is_open()) 
+    {
+        cout<< "Invalid input file. No file was opened. Please try again.\n";
+        return -1;
+    }
+    
+	else {
+        
+        vector<string> wordList;
+        std::string word = "";
+
+        
+        while (getline(in, word)) {
+            if(word.length() == wordLength){
+                wordList.push_back(word);
+            }
+        }
+	}
+	
+	srand(time(NULL));
+	int randomInt = rand() % (wordList.size() - 1);
+	
+	string wordToGuess = wordList.at(randomInt);
+	vector<char> currWord;
+	for(int i = 0; i < wordLength - 1; i++){
+	    currWord.push_back('_');
+	}
+	
+	
+	
+	char prompt = '';
+	bool gameOver = false;
+    while (!gameOver) {
+        
+        for(auto i = currWord.begin(); i != currWord.end(); i++){
+            std::cout << *i;
+        }
+        
+        
+    
+        cout << "Enter your guess" << endl;
+        getline(cin, name);
+        
+       
+       
+       
+       
+        std::cout << "Search again? (y/n)" << std::endl;
+        cin >> prompt;  // Get the first character
+        // Then ignore until newline
+        cin.ignore( numeric_limits <streamsize> ::max(), '\n' );  
+    }
+    in.close();
+
+
     return 0;
 }
